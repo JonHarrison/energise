b22f30ad809871cf4d143ef5dff9241edd655013
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-06 18:01:04 +0000

    add map markers from openchargemap API

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,8 +3,79 @@
 // https://developers.google.com/maps/documentation/javascript/examples/places-searchbox#maps_places_searchbox-html
 
 let map;
+let markers = [];
 
 const defaultGeocode = { lat: 51.509865, lon: -0.118092 }; // London
+
+function addEVMarkers(data) {
+
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+        marker.setMap(null);
+    });
+    markers = [];
+
+    var icon = new google.maps.MarkerImage('./assets/icons/charging-station-solid.svg',
+    null, null, null, new google.maps.Size(30, 30));
+
+    data.forEach(function (entry) {
+        console.log(`lat:${entry.AddressInfo.Latitude} lon:${entry.AddressInfo.Longitude} : ${entry.AddressInfo.AddressLine1},${entry.AddressInfo.AddressLine2},${entry.AddressInfo.Postcode}`);
+
+        const { AddressInfo, AddressInfo: { Latitude: lat, Longitude: lng },
+            ...rest
+        } = entry;
+
+        // Add marker
+        var marker = new google.maps.Marker({
+                // The below line is equivalent to writing:
+                // position: new google.maps.LatLng(-34.397, 150.644)
+                position: { lat: lat, lng: lng },
+                map: map,
+                draggable: false,
+                icon: icon,
+            });
+
+        // You can use a LatLng literal in place of a google.maps.LatLng object when
+        // creating the Marker object. Once the Marker object is instantiated, its
+        // position will be available as a google.maps.LatLng object. In this case,
+        // we retrieve the marker's position using the
+        // google.maps.LatLng.getPosition() method.
+        const infowindow = new google.maps.InfoWindow({
+            content: "<p>Marker Location:" + marker.getPosition() + "</p>",
+        });
+
+        google.maps.event.addListener(marker, "click", () => {
+            infowindow.open(map, marker);
+        });
+
+        markers.push(marker);
+
+    });
+
+}
+
+function retrieveEVMarkers(geocode)
+{
+    const radius = 10; // miles
+    const radiusUnits = 'miles';
+    const queryURL = `https://api.openchargemap.io/v3/poi?key={APIKey}&latitude=${geocode.lat}&longitude=${geocode.lon}&distance=${radius}&distanceunit=${radiusUnits}`;
+    const options = { method: 'GET', /*mode: 'cors',*/ headers: { 'Content-Type': 'application/json', /*'Access-Control-Request-Method': 'GET', 'Access-Control-Request-Headers': 'Content-Type, Authorization', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*'*/ } };
+    fetch(queryURL, options)
+        .then((response) => {
+            if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+        addEVMarkers(data);
+    })
+    .catch(err => {
+        console.error(err)
+        return err;
+    });
+}
 
 function initAutocomplete() {
 
@@ -20,47 +91,14 @@ function initAutocomplete() {
 
     const mapOptions = {
         center: { lat: defaultGeocode.lat, lng: defaultGeocode.lon },
-        zoom: 12,
+        zoom: 14,
         styles: mapStyles,
         mapTypeId: "roadmap" // google.maps.mapTypeId.ROADMAP,
     };
 
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var icon = {
-
-        path: "M96 0C60.7 0 32 28.7 32 64V448c-17.7 0-32 14.3-32 32s14.3 32 32 32H320c17.7 0 32-14.3 32-32s-14.3-32-32-32V304h16c22.1 0 40 17.9 40 40v32c0 39.8 32.2 72 72 72s72-32.2 72-72V252.3c32.5-10.2 56-40.5 56-76.3V144c0-8.8-7.2-16-16-16H544V80c0-8.8-7.2-16-16-16s-16 7.2-16 16v48H480V80c0-8.8-7.2-16-16-16s-16 7.2-16 16v48H432c-8.8 0-16 7.2-16 16v32c0 35.8 23.5 66.1 56 76.3V376c0 13.3-10.7 24-24 24s-24-10.7-24-24V344c0-48.6-39.4-88-88-88H320V64c0-35.3-28.7-64-64-64H96zM216.9 82.7c6 4 8.5 11.5 6.3 18.3l-25 74.9H256c6.7 0 12.7 4.2 15 10.4s.5 13.3-4.6 17.7l-112 96c-5.5 4.7-13.4 5.1-19.3 1.1s-8.5-11.5-6.3-18.3l25-74.9H96c-6.7 0-12.7-4.2-15-10.4s-.5-13.3 4.6-17.7l112-96c5.5-4.7 13.4-5.1 19.3-1.1z",
-        fillColor: '#FF0000',
-        fillOpacity: .6,
-        anchor: new google.maps.Point(0,0),
-        strokeWeight: 0,
-        scale: 1
-    };
-
-    // Add marker
-    const marker = new google.maps.Marker({
-        // The below line is equivalent to writing:
-        // position: new google.maps.LatLng(-34.397, 150.644)
-        position: { lat: defaultGeocode.lat, lng: defaultGeocode.lon },
-        map: map,
-        draggable: false,
-        icon: new google.maps.MarkerImage('./assets/icons/charging-station-solid.svg',
-            null, null, null, new google.maps.Size(30,30)),
-    });
-
-    // You can use a LatLng literal in place of a google.maps.LatLng object when
-    // creating the Marker object. Once the Marker object is instantiated, its
-    // position will be available as a google.maps.LatLng object. In this case,
-    // we retrieve the marker's position using the
-    // google.maps.LatLng.getPosition() method.
-    const infowindow = new google.maps.InfoWindow({
-        content: "<p>Marker Location:" + marker.getPosition() + "</p>",
-    });
-
-    google.maps.event.addListener(marker, "click", () => {
-        infowindow.open(map, marker);
-    });
-
+    retrieveEVMarkers(defaultGeocode);
 
     // Create the search box and link it to the UI element.
     const input = document.getElementById("pac-input");
@@ -74,7 +112,7 @@ function initAutocomplete() {
 
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
-    // searchBox.addListener("places_changed", () => {
+    searchBox.addListener("places_changed", () => {
     //     const places = searchBox.getPlaces();
 
     //     if (places.length == 0) {
@@ -87,8 +125,8 @@ function initAutocomplete() {
     //     });
     //     markers = [];
 
-    //     // For each place, get the icon, name and location.
-    //     const bounds = new google.maps.LatLngBounds();
+        // For each place, get the icon, name and location.
+        const bounds = new google.maps.LatLngBounds();
 
     //     places.forEach((place) => {
     //         if (!place.geometry || !place.geometry.location) {
@@ -120,8 +158,8 @@ function initAutocomplete() {
     //             bounds.extend(place.geometry.location);
     //         }
     //     });
-    //     map.fitBounds(bounds);
-    // });
+        map.fitBounds(bounds);
+    });
 
 }
 
