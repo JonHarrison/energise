95b0153f09d843cae350e0f69336abf1f1d000d8
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-10 22:17:56 +0000

    fix for displaying ev charging points update after map search

--- assets/js/index.js
+++ assets/js/index.js
@@ -114,52 +114,38 @@ function initAutocomplete() {
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     searchBox.addListener("places_changed", () => {
-    //     const places = searchBox.getPlaces();
+        const places = searchBox.getPlaces();
 
-    //     if (places.length == 0) {
-    //         return;
-    //     }
+        if (places.length == 0) {
+            return;
+        }
 
-    //     // Clear out the old markers.
-    //     markers.forEach((marker) => {
-    //         marker.setMap(null);
-    //     });
-    //     markers = [];
+        // Clear out the old markers.
+        markers.forEach((marker) => {
+            marker.setMap(null);
+        });
+        markers = [];
 
-        // For each place, get the icon, name and location.
+
+        // For each place, get the location / viewport and extend the bounds.
+        var geocode;
         const bounds = new google.maps.LatLngBounds();
+        places.forEach((place) => {
+            if (!place.geometry || !place.geometry.location) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
 
-    //     places.forEach((place) => {
-    //         if (!place.geometry || !place.geometry.location) {
-    //             console.log("Returned place contains no geometry");
-    //             return;
-    //         }
-
-    //         const icon = {
-    //             url: place.icon,
-    //             size: new google.maps.Size(71, 71),
-    //             origin: new google.maps.Point(0, 0),
-    //             anchor: new google.maps.Point(17, 34),
-    //             scaledSize: new google.maps.Size(25, 25),
-    //         };
-
-    //         // Create a marker for each place.
-    //         markers.push(
-    //             new google.maps.Marker({
-    //                 map,
-    //                 icon,
-    //                 title: place.name,
-    //                 position: place.geometry.location,
-    //             })
-    //         );
-    //         if (place.geometry.viewport) {
-    //             // Only geocodes have viewport.
-    //             bounds.union(place.geometry.viewport);
-    //         } else {
-    //             bounds.extend(place.geometry.location);
-    //         }
-    //     });
-        map.fitBounds(bounds);
+            geocode = {lat: place.geometry.location.lat() , lon : place.geometry.location.lng() } ;
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds); // update the map to the new location
+        retrieveEVMarkers(geocode); // get the new EV locations
     });
 
 }
