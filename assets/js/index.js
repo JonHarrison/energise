fd9d8b76de8362b27e6f1736cd3e8b08a86bd86e
Jon Harrison <jonathan_harrison@hotmail.com>
2023-01-10 22:26:34 +0000

    arrange page layout into pac-card and pac-container as per Google css, add infowindow div

--- assets/js/index.js
+++ assets/js/index.js
@@ -110,10 +110,11 @@ function initAutocomplete() {
     retrieveEVMarkers(defaultGeocode);
 
     // Create the search box and link it to the UI element.
+    const card = document.getElementById("pac-card");
     const input = document.getElementById("pac-input");
     const searchBox = new google.maps.places.SearchBox(input);
 
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
     // Bias the SearchBox results towards current map's viewport.
     map.addListener("bounds_changed", () => {
         searchBox.setBounds(map.getBounds());
